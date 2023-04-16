52d7e043357324489defc2547592285d8b26fc94
chobolevel <rodaka123@naver.com>
2023-04-16 20:44:00 +0900

    2023-04-16

---
+++ src/main/java/kr/co/deco/house/entity/product/Product.java
@@ -0,0 +1,12 @@
+package kr.co.deco.house.entity.product;
+
+import kr.co.deco.house.entity.Base;
+
+public class Product extends Base {
+
+    private String id;
+    private String title;
+    private String desc;
+    private String mainImg;
+
+}
---
+++ src/main/java/kr/co/deco/house/mapper/product/ProductMapper.java
@@ -0,0 +1,10 @@
+package kr.co.deco.house.mapper.product;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProductMapper {
+
+
+
+}
